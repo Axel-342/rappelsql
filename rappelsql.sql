16ed1f4cce40cb94da0a5bcd442c11514c54c7d4
Axel-342 <axel.mouisson@gmail.com>
2024-10-03 14:35:54 +0200

    question 5

--- rappelsql.sql
+++ rappelsql.sql
@@ -5,3 +5,5 @@ select nom, email from Clients
 select * from Clients where age > 40
 
 select * from Clients where location = 'Paris' and age < 35 and age > 25
+
+select * from Campagnes order by budget
